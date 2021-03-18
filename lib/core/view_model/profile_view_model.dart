1c71a0cbb9c48d6ea9699d2e212c01d24e96b7f9
Mostafa Ahmed <40537665+MOstafaZaRiaa@users.noreply.github.com>
2021-03-18 17:38:56 +0200

    Update profile_view_model.dart

--- lib/core/view_model/profile_view_model.dart
+++ lib/core/view_model/profile_view_model.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerce_app/model/user_model.dart';
 import 'package:get/get.dart';
 
 import 'package:google_sign_in/google_sign_in.dart';
@@ -5,9 +6,24 @@ import 'package:firebase_auth/firebase_auth.dart';
 
 import 'package:ecommerce_app/helper/local_storage_data.dart';
 
-class ProfileViewModel extends GetxController{
+class ProfileViewModel extends GetxController {
+  UserModel _userModel;
+
+  UserModel get userModel => _userModel;
   final LocalStorageData localStorageData = Get.find();
-  signOutOffAll(){
+
+  onInit() {
+    super.onInit();
+    getUserData();
+  }
+
+  getUserData() {
+    localStorageData.getUser.then((value) => {
+          _userModel = value,
+        });
+  }
+
+  signOutOffAll() {
     FirebaseAuth.instance.signOut();
     GoogleSignIn().signOut();
     localStorageData.deleteUser();
